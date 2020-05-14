3fb7e218699b0d494fe8b60968c80607441d3037
Amresh S Kumar <Amresh S Kumar@AMRESH>
2020-05-14 07:56:09 +0530

    URl mappings updated

--- MobileStoreRestApp/src/main/java/com/amresh/restapp/mobilestorerestapp/controller/MobileController.java
+++ MobileStoreRestApp/src/main/java/com/amresh/restapp/mobilestorerestapp/controller/MobileController.java
@@ -1,17 +1,20 @@
 package com.amresh.restapp.mobilestorerestapp.controller;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.amresh.restapp.mobilestorerestapp.model.Mobiles;
 import com.amresh.restapp.mobilestorerestapp.service.MobileService;
 
 @RestController
+@RequestMapping("/mobiles")
 public class MobileController {
 
 	@Autowired
@@ -23,12 +26,12 @@ public class MobileController {
 		return "Mobile Added Successfully";
 	}
 
-	@GetMapping("/findAllMobiles")
+	@GetMapping("/getAll")
 	public List<Mobiles> getAllMobiles() {
 		return service.findAll();
 	}
 
-	@GetMapping("/findMobilesByModelName/{modelName}")
+	@GetMapping("/{modelName}")
 	public List<Mobiles> getAllByModelName(@PathVariable String modelName) {
 		return service.findByModelName(modelName);
 	}
